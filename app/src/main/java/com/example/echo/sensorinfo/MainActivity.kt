4d74f7efb34969c75b2feb94647bafa5063f0b6e
heikexiaosong@gmail.com <Huoli6688>
2018-05-24 14:49:43 +0800

    启动另一个 Activity

--- app/src/main/java/com/example/echo/sensorinfo/MainActivity.kt
+++ app/src/main/java/com/example/echo/sensorinfo/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.echo.sensorinfo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.EditText
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val  EXTRA_MESSAGE:String = "com.example.myfirstapp.MESSAGE"
+        val  LOAN_TITLE = "loanTitle"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button  */
+    fun sendMessage(view: View) {
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        val editText = findViewById<View>(R.id.editText) as EditText
+        val message = editText.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
+        startActivity(intent)
     }
 }
